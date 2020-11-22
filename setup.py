2b8e566114a4f1878ece6a352462416045ca992c
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-11-22 14:44:49 +0200

    0.0.5

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="libsearch", # Replace with your own username
-    version="0.0.4",
-    author="jbin",
+    version="0.0.5",
+    author="Ioannis Biniaris",
     author_email="john_biniaris@hotmail.com",
     description="A library of searh algorithms",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/Johnbin89/SearchLibrary",
     packages=setuptools.find_namespace_packages(),
     #packages=['search'],
     #package_dir={'': 'src'},
